f1a3ca4abb0cbdd95c14828872d120ddfa58e3aa
Eric Halvorsen <eric.halvorsen@arcadiasolutions.com>
2012-12-17 15:22:23 -0500

    Routing to homepage and dashboard depending on signed in / not signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Shouter::Application.routes.draw do
+  constraints Clearance::Constraints::SignedIn.new do
+    root :to => "dashboards#show"
+  end
+  
+  root :to => "home#show"
+  
   resource :dashboard, only: [:show]
   resources :text_shouts, only: [:create]
   resources :photo_shouts, only: [:create]
+  resources :users, only: [:show]
 end
