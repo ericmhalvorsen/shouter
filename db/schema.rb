6f22c2cd9813c55aa0b4528856b720ddcbd51527
Eric Halvorsen <eric.halvorsen@arcadiasolutions.com>
2012-12-17 11:38:35 -0500

    added Textshout and ran migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121217145835) do
+ActiveRecord::Schema.define(:version => 20121217163739) do
 
   create_table "shouts", :force => true do |t|
     t.string   "body"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(:version => 20121217145835) do
   end
 
   add_index "shouts", ["user_id"], :name => "index_shouts_on_user_id"
+
+  create_table "text_shouts", :force => true do |t|
+    t.string   "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email"
