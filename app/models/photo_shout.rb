f1170e0882b6d5c6e33185206f939cdd06c0082f
Eric Halvorsen <eric.halvorsen@arcadiasolutions.com>
2012-12-17 14:29:50 -0500

    Updating gitignore, modifying views, making images display.

--- app/models/photo_shout.rb
+++ app/models/photo_shout.rb
@@ -1,3 +1,5 @@
 class PhotoShout < ActiveRecord::Base
   # attr_accessible :title, :body
+  has_attached_file :image
+  attr_accessible :image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Shouter::Application.routes.draw do
   resource :dashboard, only: [:show]
   resources :text_shouts, only: [:create]
+  resources :photo_shouts, only: [:create]
 end
